e9bf45e590aef0819db0955b9d9884bb0192801f
3r1s_s <127928354+3r1s-s@users.noreply.github.com>
2024-07-09 16:56:31 -0700

    yes

--- meoads.js
+++ meoads.js
@@ -1,12 +1,10 @@
 javascript:(function() {
   var a = document.createElement("div");
     a.innerHTML = `
-<center style="position: absolute;bottom:0;left:0;right:0;">
+<div style="position: fixed;bottom:0;left:0;right:0;display: flex;justify-content: center;align-items: center;">
     <iframe id="BetterAd" src="https://adservice.bettermeower.app/generate" style="border: medium; width: 728px; height: 90px; overflow: hidden;" scrolling="no"></iframe>
     <iframe id="BetterAd" src="https://adservice.bettermeower.app/generate" style="border: medium; width: 728px; height: 90px; overflow: hidden;" scrolling="no"></iframe>
-    <iframe id="BetterAd" src="https://adservice.bettermeower.app/generate" style="border: medium; width: 728px; height: 90px; overflow: hidden;" scrolling="no"></iframe>
-    <iframe id="BetterAd" src="https://adservice.bettermeower.app/generate" style="border: medium; width: 728px; height: 90px; overflow: hidden;" scrolling="no"></iframe>
-</center>
+</div>
     `
   document.body.appendChild(a);
 })();
