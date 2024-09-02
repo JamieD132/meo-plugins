2fcf6447e4bab6e5d257f09a6838e77a9001d146
3r1s_s <127928354+3r1s-s@users.noreply.github.com>
2024-09-02 14:25:52 -0700

    ulist crown

--- crown.js
+++ crown.js
@@ -1,1 +1,9 @@
-window.alert("Please disable this plugin ill fix later");
+function ulistcrown() {
+    const elements = document.querySelectorAll(`[data-user-title="${sul.split(", ")[0]}"]`);
+    elements.forEach(element => {
+        element.innerText = `${sul.split(", ")[0]}👑`;
+    });
+}
+
+// this was the best i could do, if anyone has a better solution please let me know
+setInterval(ulistcrown, 500);
