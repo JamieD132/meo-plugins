fb5bfa085040e980964f3206ae95718867da36d5
Arrow <89811520+ArrowAced@users.noreply.github.com>
2024-10-03 22:14:18 -0400

    Create flashlight.js

---
+++ flashlight.js
@@ -0,0 +1,28 @@
+const flashstyle = document.createElement('style');
+flashstyle.textContent = `
+  body:after {
+    content: '';
+    position: fixed;
+    inset: -50vmax;
+    z-index: 7;
+    pointer-events: none;
+
+    background-image: radial-gradient(circle, transparent -100px, var(--background) 200px);
+    background-position: var(--flashx) var(--flashy);
+    background-repeat: no-repeat;
+    }
+
+  .modal-back {
+    z-index: 8;
+  }
+`;
+document.head.appendChild(flashstyle);
+
+
+function flashlight(e) {
+  document.body.style.setProperty("--flashx", `${e.clientX - window.innerWidth / 2}px`);
+  document.body.style.setProperty("--flashy", `${e.clientY - window.innerHeight / 2}px`);
+}
+
+addEventListener("mousemove", flashlight);
+addEventListener("touchmove", (e) => {flashlight(e.changedTouches[0])});
